32eea2b04d65c1fb4bb01b9374eec42693003c9f
sahandedc <sahandedc@gmail.com>
2024-11-20 13:23:30 +0330

    url

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import { Route, Routes} from "react-router-dom";
 import MoviesShow from "./components/MoviesShow";
 import getData from "./components/getDataMovie";
-// import { createBrowserRouter } from "react-router-dom";
 const App = () => {
-  // const routes = createBrowserRouter(routes, {
-  // future: {
-  // v7_partialHydration: true,
-  // },
-  
-  // });
+  console.log("object");
   return (
     <>
-
       <Routes>
-        <Route path="/test2/"  Component={getData} />
+        <Route path="/test2/" excite Component={getData} />
         <Route path="/test2/"  Component={MoviesShow} />
         <Route path="/:id"  Component={getData} />
       </Routes>
